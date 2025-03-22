4ce5160abaf7308808a7b919ec659b6eb8cc005e
anujbhatt-dev <anujbhatt023@gmail.com>
2025-03-22 19:13:40 +0400

    feat: product almost done

--- components/SimilarProduct.tsx
+++ components/SimilarProduct.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Heading from './Heading'
-import ProductThumbnail from './ProductThumbnail'
 import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
 import ProductGrid from './ProductGrid';
 
